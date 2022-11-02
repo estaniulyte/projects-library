a9406c8513d6177b1538847f59ec83ab180c5da9
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-11-02 18:41:44 +0200

    feat: add home page component

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+    <HomeAppBar />
+    <v-container>
+      <v-row class="d-flex justify-space-between">
+        <h1 class="pl-2">Projects</h1>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
+	</div>
+</template>
+
+<script>
+import HomeAppBar from "../components/HomeAppBar.vue";
+
+export default {
+    name: "HomePage",
+    data: () => ({}),
+    components: { HomeAppBar }
+}
+</script>
+
+<style scoped>
+</style>
