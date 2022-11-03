98537d2a768e2bf54886386c314d8db5fb996ebb
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-11-03 23:17:54 +0200

    feat: add search box

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,22 @@
 <template>
 	<div>
     <HomeAppBar />
+    <div class="background-bar" />
+    <v-row class="d-flex justify-center">
+      <div class="search-box mx-6">
+        <v-text-field
+          v-model="searchInput"
+          prepend-inner-icon="mdi-magnify"
+          class="transition-swing ma-0 pa-0"
+          label="Search..."
+          hide-details
+          single-line
+          justify-sm
+          rounded
+          clearable
+        />
+      </div>
+    </v-row>
     <v-container>
       <v-row class="d-flex justify-space-between">
         <h1 class="pl-2">Projects</h1>
@@ -20,5 +36,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.background-bar {
+  max-width: 100%;
+  min-width: 0px;
+  width: 100%;
+  height: 80px;
+  background: #f3f3f4;
+  display: block;
+}
+
+.search-box {
+  display: flex;
+  width: 630px;
+  height: 64px;
+  position: relative;
+  background-color: white;
+  transform: translateY(-20px);
+  z-index: 2;
+  align-items: center;
+  box-shadow: 0px 8px 20px rgb(0 0 0 / 6%);
+  border-radius: 8px;
+}
 </style>
