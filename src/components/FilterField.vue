42578f39c6da31b9bb82477482f5e43946d0c319
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-11-06 23:28:03 +0200

    feat: create a Filter Field component

---
+++ src/components/FilterField.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-col
+    class="px-3 px-sm-7"
+    cols="12"
+    xl="3"
+    lg="3"
+    md="6"
+    sm="6"
+  >
+    <v-row class="font-weight-medium">
+      {{ title }}
+    </v-row>
+    <v-row>
+      <v-autocomplete
+        v-model="selectedProperties"
+        :items="properties"
+        class="transition-swing py-2 lol"
+        filled
+        item-value="id"
+        item-text="name"
+        multiple
+        small-chips
+        return-object
+        hide-selected
+        deletable-chips
+        outlined
+        dense
+        hide-details
+      />
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+export default {
+	name: "FilterField",
+  props: {
+    properties: {
+      type: Array,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    }
+  },
+	data: () => ({
+	})
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .theme--light.v-text-field--filled > .v-input__control > .v-input__slot{
+  background: rgba(0, 0, 0, 0.03);
+  padding: 4px 0x;
+}
+
+::v-deep .theme--light.v-chip:not(.v-chip--active) {
+  background: #e1e8e5;
+  margin: 4px;
+}
+</style>
