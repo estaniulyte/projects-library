8d9934612960088eaa213c973ef44ea766d1072d
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-12-12 22:01:00 +0200

    feat: create separate Avatar Button component

---
+++ src/components/AvatarButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-menu
+    bottom
+    min-width="200px"
+    rounded
+    offset-y
+    elevation="0"
+    transition="slide-y-transition"
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn
+        icon
+        x-large
+        v-on="on"
+      >
+        <v-avatar :size=imageSize>
+          <img
+            src="https://i.pravatar.cc/43"
+            alt="Slack Image"
+          >
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-card
+      elevation="0"
+    >
+      <v-list-item-content class="justify-center" elevation="0">
+        <div class="mx-auto text-center">
+          <template
+            v-for="(item, index) of menuButtons"
+          >
+            <v-btn
+              @click="item.action ? item.action() : () => {}"
+              :key="index"
+              v-bind="item.bind"
+              small
+              text
+            >
+              {{ item.name }}
+            </v-btn>
+            <v-divider
+              v-if="menuButtons.length !== index+1"
+              :key="index"
+              class="my-3"
+            />
+          </template>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+
+export default {
+	name: "AvatarButton",
+  props: {
+    imageSize: {
+      type: String,
+      default: '43px'
+    }
+  },
+	data: () => ({
+    menuButtons: [
+      {
+        name: 'Owned projects'
+      },
+      {
+        name: 'Logout'
+      }
+    ],
+	})
+}
+</script>
