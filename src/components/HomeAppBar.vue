722b3a8ebb3fc182bc4244e1abb7a936f50a051a
Evelina Staniulytė <e.staniulyte98@gmail.com>
2022-12-12 22:11:58 +0200

    feat: improve responsiveness

--- src/components/HomeAppBar.vue
+++ src/components/HomeAppBar.vue
@@ -1,107 +1,79 @@
 <template>
   <v-app-bar
-    class="px-4"
+    class="px-1 px-sm-4"
     elevation="0"
     color="white"
   >
-    <router-link to="/">Projects Library</router-link>
-    <v-spacer />
-
-    <v-btn
-      v-for="(item, index) of actionButtons"
-      :key="index"
-      color="primary"
-      class="mr-2"
-      @click="item.action ? item.action() : () => {}"
-      v-bind="item.bind"
-      outlined
-      fab
-      small
-    >
-      <v-tooltip bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-icon :size="item.iconSize"  v-bind="attrs"
-            v-on="on">
-            {{ item.iconName }}
-          </v-icon>
-        </template>
-        <span> {{ item.tootlip }} </span>
-      </v-tooltip>
-    </v-btn>
-    <v-btn
-      class="px-7 py-5 mr-3"
-      color="primary"
-      v-bind="attrs"
-      elevation="0"
-      small
-    >
-      <span class="ml-2">Add project</span>
-    </v-btn>
-    <v-menu
-      bottom
-      min-width="200px"
-      rounded
-      offset-y
-      elevation="0"
-      transition="slide-y-transition"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          icon
-          x-large
-          v-on="on"
-        >
-          <v-avatar size="43px">
-            <img
-              src="https://i.pravatar.cc/43"
-              alt="Slack Image"
-            >
-          </v-avatar>
-        </v-btn>
-      </template>
-      <v-card
+    <v-row class="d-none d-sm-inline-flex align-center">
+      <router-link to="/">Project Repository</router-link>
+      <v-spacer />
+      <v-btn
+        v-for="(item, index) of actionButtons"
+        :key="index"
+        color="primary"
+        class="mx-1"
+        @click="item.action ? item.action() : () => {}"
+        v-bind="item.bind"
+        outlined
+        fab
+        small
+      >
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon :size="item.iconSize"  v-bind="attrs"
+              v-on="on">
+              {{ item.iconName }}
+            </v-icon>
+          </template>
+          <span> {{ item.tootlip }} </span>
+        </v-tooltip>
+      </v-btn>
+      <v-btn
+        class="px-7 py-5 mx-3"
+        color="primary"
         elevation="0"
+        small
       >
         <span class="ml-2">Add project</span>
       </v-btn>
       <AvatarButton />
+    </v-row>
+    <v-row class="d-sm-none d-flex align-center ma-0">
+      <router-link to="/">Project Repository</router-link>
+      <v-spacer />
+      <AvatarButton imageSize='40px' />
+    </v-row>
   </v-app-bar>
 </template>
 
 <script>
+import AvatarButton from './AvatarButton.vue';
 
 export default {
-	name: "HomeAppBar",
-	data: () => ({
-    actionButtons: [
-        name: 'Slack Channel',
-        iconName: 'mdi-slack',
-        iconSize: '19',
-        tootlip: 'Feedback & Questions',
-        bind: {
-          href: 'slack://'
+    name: "HomeAppBar",
+    data: () => ({
+      actionButtons: [
+        {
+          name: "Slack Channel",
+          iconName: "mdi-slack",
+          iconSize: "19",
+          tootlip: "Feedback & Questions",
+          bind: {
+            href: "slack://"
+          }
+        },
+        {
+          name: "Documentation",
+          iconName: "mdi-help",
+          iconSize: "17",
+          tootlip: "Documentation",
+          bind: {
+            href: "",
+            target: "_blank"
+          }
         }
-      },
-      {
-        name: 'Documentation',
-        iconName: 'mdi-help',
-        iconSize: '17',
-        tootlip: 'Documentation',
-        bind: {
-          href: '',
-          target: '_blank'
-        }
-      }
-    ],
-    menuButtons: [
-      {
-        name: 'Owned projects'
-      },
-      {
-        name: 'Logout'
-      }
-    ]
-	})
+      ],
+    }),
     components: {
       AvatarButton,
     }
