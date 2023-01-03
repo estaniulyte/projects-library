025a3391765901dbc1bfe9fcd753e42d53bad04f
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-03 15:46:05 +0200

    feat: create Project page component

---
+++ src/pages/Project.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HomeAppBar />
+  </div>
+</template>
+
+<script>
+import HomeAppBar from '@/components/HomeAppBar.vue';
+
+export default {
+    name: "ProjectPage",
+    components: { HomeAppBar }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
