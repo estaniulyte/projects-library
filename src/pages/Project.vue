cf1d274dc3fa02ee0dc8cd745bd3df18b0bee3c1
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-08 22:12:52 +0200

    feat: get project by router params id

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -6,10 +6,18 @@
 
 <script>
 import HomeAppBar from '@/components/HomeAppBar.vue';
+import projectsData from "@/assets/data/projects_detailed.json"
 
 export default {
     name: "ProjectPage",
     components: { HomeAppBar }
+    data: () => ({
+      project: {},
+      projects: projectsData
+    }),
+    created () {
+      this.project = this.projects.find((e) => e.id === this.$route.params.id)
+    },
 }
 </script>
 
