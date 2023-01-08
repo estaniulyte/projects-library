68c2846b32a0701e137851b4b1565968f1cd0b85
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-08 22:13:10 +0200

    feat: add projects banner

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
     <HomeAppBar />
+    <div v-if="project">
+      <ProjectBanner :project="project" />
+    </div>
   </div>
 </template>
 
 <script>
-import HomeAppBar from '@/components/HomeAppBar.vue';
+import HomeAppBar from '@/components/HomeAppBar.vue'
+import ProjectBanner from '@/components/projectPageComponents/Banner.vue'
+
 import projectsData from "@/assets/data/projects_detailed.json"
 
 export default {
     name: "ProjectPage",
-    components: { HomeAppBar }
     data: () => ({
       project: {},
       projects: projectsData
     }),
+    components: { 
+      HomeAppBar,
+      ProjectBanner
+    },
     created () {
       this.project = this.projects.find((e) => e.id === this.$route.params.id)
     },
