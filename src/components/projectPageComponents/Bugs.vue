ce132f3af2c75066b9f9e5aaca9816d764566706
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-10 23:21:48 +0200

    feat: create Bugs tab component

---
+++ src/components/projectPageComponents/Bugs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-row class="ma-2">
+      <v-alert
+        v-if="!bugs.length"
+        class="my-2 py-3"
+        width="100%"
+        icon="mdi-information-variant"
+        prominent
+        dense
+        text
+        color="blue-grey darken-1"
+      >
+        <v-row class="d-flex justify-space-between mx-2">
+          <v-col class="d-flex align-center">Project has no reported bugs.</v-col>
+          <v-col class="d-flex justify-end">
+            <v-btn
+              class="px-7 py-5"
+              color="primary lighten-1"
+              elevation="0"
+              small
+            >
+              Sumbit bug
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
+    </v-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProjectBugs",
+    props: {
+      bugs: {}
+    },
+    data: () => ({
+    })
+  }
+</script>
