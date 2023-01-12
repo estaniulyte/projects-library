a54f305281a79d078a6fb202f86d1ec57b2bcdc8
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-12 17:41:31 +0200

    feat: create Tested Versions component

---
+++ src/components/projectPageComponents/TestedVersions.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="d-flex">
+      TestedVersions
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectTestedVersions",
+  props: {
+    versions: {},
+    sourceVersion: {},
+  }
+}
+</script>
