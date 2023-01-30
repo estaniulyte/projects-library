ebf8f0fec38bba781043192cb13ef4552f344f81
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-01-30 22:40:07 +0200

    feat: create comments section

---
+++ src/components/projectPageComponents/Comments.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pb-8">
+    <v-row class="pt-5">
+      <v-col>
+        <h3 class="font-weight-light">
+          COMMENTS
+        </h3>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectComments",
+}
+</script>
