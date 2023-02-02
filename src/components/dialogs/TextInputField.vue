df58479d7ea061d66b7fe6e45d9d6befbc774087
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-02-02 19:12:27 +0200

    feat: create Text Input Field component

---
+++ src/components/dialogs/TextInputField.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="">
+    <div class="caption">{{ title }}</div>
+    <v-text-field
+      class="ma-0 pa-1 input px-0"
+      :label="label"
+      hide-details
+      single-line
+      justify-sm
+      rounded
+      clearable
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextInputField",
+  props: {
+    title: String,
+    label: String
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.input {
+  background-color: rgba(231, 238, 233, 0.625);
+  border-radius: 6px;
+  font-size: small;
+}
+
+::v-deep .v-input__slot {
+  padding: 2px 15px!important;
+}
+
+::v-deep .v-label {
+  font-size: small!important;
+}
+
+</style>
