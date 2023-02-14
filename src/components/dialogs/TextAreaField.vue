16fa8250808a1d37b10a50f1896bd9004902ac69
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-02-14 23:14:01 +0200

    feat: create Text Area Field component

---
+++ src/components/dialogs/TextAreaField.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="field">
+    <div class="caption">{{ title }}<span v-if="required" class="red--text"> *</span></div>
+    <v-textarea
+      class="ma-0 pa-1 input px-0"
+      :label="label"
+      :placeholder="label"
+      rows="1"
+      hide-details
+      single-line
+      justify-sm
+      rounded
+      auto-grow
+      clearable
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextAreaField",
+  props: {
+    title: String,
+    label: String,
+    required: {
+      type: Boolean,
+      default: false
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.input {
+  background-color: rgba(231, 238, 233, 0.625);
+  border-radius: 6px;
+  font-size: small;
+}
+
+::v-deep .v-input__slot {
+  padding: 2px 15px!important;
+}
+
+::v-deep .v-label {
+  font-size: small!important;
+}
+
+.field {
+  width: 100%;
+}
+
+</style>
