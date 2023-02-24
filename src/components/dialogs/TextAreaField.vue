71e184953111af150ef78204cdb13be97f802bb2
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-02-24 18:00:42 +0200

    change: add two buttons above text area field

--- src/components/dialogs/TextAreaField.vue
+++ src/components/dialogs/TextAreaField.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="field">
-    <div class="caption">{{ title }}<span v-if="required" class="red--text"> *</span></div>
+    <div class="d-flex justify-space-between">
+      <div class="caption d-flex align-end">{{ title }}<span v-if="required" class="red--text"> *</span></div>
+      <div class=" pb-1 caption grey--text text--darken-1" v-if="additionalInfo">
+       <!-- {{ additionalInfo }} -->
+       <v-btn fab x-small height="25px" width="25px" elevation="0" text>
+        <v-icon size="17px" color="primary">mdi-text-box-edit-outline</v-icon>
+       </v-btn>
+       <v-btn class="ml-1" fab x-small height="25px" width="25px" elevation="0" text>
+        <v-icon size="17px" color="primary">mdi-text-box-plus-outline</v-icon>
+       </v-btn>
+      </div>
+    </div>
     <v-textarea
       class="ma-0 pa-1 input px-0"
       :label="label"
       :placeholder="label"
-      rows="1"
+      rows="3"
       hide-details
       single-line
       justify-sm
@@ -22,6 +33,7 @@ export default {
   props: {
     title: String,
     label: String,
+    additionalInfo: String,
     required: {
       type: Boolean,
       default: false
@@ -36,6 +48,7 @@ export default {
   background-color: rgba(231, 238, 233, 0.625);
   border-radius: 6px;
   font-size: small;
+  line-height: 0.1rem!important;
 }
 
 ::v-deep .v-input__slot {
@@ -50,4 +63,7 @@ export default {
   width: 100%;
 }
 
+::v-deep .v-textarea textarea{
+  line-height: 1rem!important;
+}
 </style>
