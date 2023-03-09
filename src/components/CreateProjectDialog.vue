25a37853d610adca40a24d2a73b47a3c3b12f18a
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-03-09 18:38:47 +0200

    feat: add form valitadion

--- src/components/CreateProjectDialog.vue
+++ src/components/CreateProjectDialog.vue
@@ -15,25 +15,30 @@
       <v-container class="pa-4 px-7">
         <h3 class="py-3">Add project</h3>
         <v-divider class="pb-3" />
-        <div class="d-flex flex-column justify-start mb-3">
-          <TextInputField title="Title" label="Project name" class="my-2" :required="true" />
-          <TextInputField title="Link" label="https//github.com/project-link" class="my-2" :required="true" />
-          <div class="d-flex flex-row">
-            <SelectInputField title="Render Pipeline" label="Select Render Pipelines" class="my-2 mr-2" :required="true" />
-            <SelectInputField title="Platform" label="Select supported platforms" class="my-2" :required="true" />
+        <v-form
+          ref="form"
+          v-model="valid"
+        >
+          <div class="d-flex flex-column justify-start mb-3">
+            <TextInputField title="Title" label="Project name" class="my-2" :required="true" />
+            <TextInputField title="Link" label="https//github.com/project-link" class="my-2" :required="true" />
+            <div class="d-flex flex-row">
+              <SelectInputField title="Render Pipeline" label="Select Render Pipelines" class="my-2 mr-2" :required="true" />
+              <SelectInputField title="Platform" label="Select supported platforms" class="my-2" :required="true" />
+            </div>
+            <SelectInputField title="Project owners" label="Select owners" class="my-2" />
+            <div class="d-flex flex-row">
+              <SelectInputField title="Primary Features" label="Select up to 5" class="my-2 mr-2" :required="true" />
+              <SelectInputField title="Features" label="Other project features" class="my-2" />
+            </div>
+            <div class="d-flex flex-row">
+              <SelectInputField title="Project version in source" label="Select Unity Stream" class="my-2 mr-2" :required="true" />
+              <TextInputField title="Unity version" label="E.g. 0f12" class="my-2" :required="true" />
+            </div>
+            <TextAreaField title="Description" label="Plain text or markdown" class="my-2" additionalInfo="Paste template" />
+            <FileInputField title="Project cover image" label="Plain text or markdown" additionalInfo="max size 2MB" class="my-2" />
           </div>
-          <SelectInputField title="Project owners" label="Select owners" class="my-2" />
-          <div class="d-flex flex-row">
-            <SelectInputField title="Primary Features" label="Select up to 5" class="my-2 mr-2" :required="true" />
-            <SelectInputField title="Features" label="Other project features" class="my-2" />
-          </div>
-          <div class="d-flex flex-row">
-            <SelectInputField title="Project version in source" label="Select Unity Stream" class="my-2 mr-2" :required="true" />
-            <TextInputField title="Unity version" label="E.g. 0f12" class="my-2" :required="true" />
-          </div>
-          <TextAreaField title="Description" label="Plain text or markdown" class="my-2" additionalInfo="Paste template" />
-          <FileInputField title="Project cover image" label="Plain text or markdown" additionalInfo="max size 2MB" class="my-2" />
-        </div>
+        </v-form>
         <v-divider class="pb-3" />
         <div class="d-flex flex-row">
           <v-btn
@@ -44,6 +49,7 @@
           </v-btn>
           <v-spacer></v-spacer>
           <v-btn
+            :disabled="!valid"
             class="px-6 py-5"
             color="primary lighten-1"
             elevation="0"
@@ -66,7 +72,8 @@ import FileInputField from '@/components/dialogs/FileInputField.vue'
 export default {
     name: "CreateProjectDialog",
     data: () => ({
-      loading: false
+      loading: false,
+      valid: false,
     }),
     props: {
       value: Boolean
