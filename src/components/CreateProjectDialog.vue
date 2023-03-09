a05c230889270b62b585a7fcf766e16253df36d9
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-03-09 18:41:14 +0200

    feat: add rules to New Project form

--- src/components/CreateProjectDialog.vue
+++ src/components/CreateProjectDialog.vue
@@ -20,23 +20,23 @@
           v-model="valid"
         >
           <div class="d-flex flex-column justify-start mb-3">
-            <TextInputField title="Title" label="Project name" class="my-2" :required="true" />
-            <TextInputField title="Link" label="https//github.com/project-link" class="my-2" :required="true" />
+            <TextInputField title="Title" label="Project name" class="my-2" :required="true" :rules="titleRules" />
+            <TextInputField title="Link" label="https//github.com/project-link" class="my-2" :required="true" :rules="linkRules" />
             <div class="d-flex flex-row">
-              <SelectInputField title="Render Pipeline" label="Select Render Pipelines" class="my-2 mr-2" :required="true" />
-              <SelectInputField title="Platform" label="Select supported platforms" class="my-2" :required="true" />
+              <SelectInputField title="Render Pipeline" label="Select Render Pipelines" class="my-2 mr-2" :required="true"  :rules="renderPipelineRules" />
+              <SelectInputField title="Platform" label="Select supported platforms" class="my-2" :required="true" :rules="platformRules" />
             </div>
             <SelectInputField title="Project owners" label="Select owners" class="my-2" />
             <div class="d-flex flex-row">
-              <SelectInputField title="Primary Features" label="Select up to 5" class="my-2 mr-2" :required="true" />
+              <SelectInputField title="Primary Features" label="Select up to 5" class="my-2 mr-2" :required="true" :rules="tagsRules" />
               <SelectInputField title="Features" label="Other project features" class="my-2" />
             </div>
             <div class="d-flex flex-row">
-              <SelectInputField title="Project version in source" label="Select Unity Stream" class="my-2 mr-2" :required="true" />
-              <TextInputField title="Unity version" label="E.g. 0f12" class="my-2" :required="true" />
+              <SelectInputField title="Project version in source" label="Select Unity Stream" class="my-2 mr-2" :required="true" :rules="unityStreamRules" />
+              <TextInputField title="Unity version" label="E.g. 0f12" class="my-2" :required="true" :rules="unityVersionRules" />
             </div>
             <TextAreaField title="Description" label="Plain text or markdown" class="my-2" additionalInfo="Paste template" />
-            <FileInputField title="Project cover image" label="Plain text or markdown" additionalInfo="max size 2MB" class="my-2" />
+            <FileInputField title="Project cover image" label="Plain text or markdown" additionalInfo="max size 2MB" class="my-2" :rules="imageRules" />
           </div>
         </v-form>
         <v-divider class="pb-3" />
@@ -74,6 +74,37 @@ export default {
     data: () => ({
       loading: false,
       valid: false,
+      titleRules: [
+        v => v !== ' ' || 'Name is required',
+        v => v !== null || 'Name is required',
+        v => v !== '' || 'Name is required'
+      ],
+      linkRules: [
+        v => v !== ' ' || 'Link is required',
+        v => v !== null || 'Link is required',
+        v => v !== '' || 'Link is required'
+      ],
+      renderPipelineRules: [
+        v => v.length !== 0 || 'Render Pipeline is required'
+      ],
+      platformRules: [
+        v => v.length !== 0 || 'Supported platform is required'
+      ],
+      tagsRules: [
+        v => v.length !== 0 || 'Primary features are required',
+        v => v.length <= 5 || 'Max 5 primary features are allowed'
+      ],
+      unityStreamRules: [
+        v => v > 0 || 'Version is required'
+      ],
+      unityVersionRules: [
+        v => v !== ' ' || 'Unity version is required',
+        v => v !== null || 'Unity version is required',
+        v => v !== '' || 'Unity version is required'
+      ],
+      imageRules: [
+        value => !value || value.size < 2000000 || 'Project image size should be less than 2 MB!' // 700KB
+      ],
     }),
     props: {
       value: Boolean
