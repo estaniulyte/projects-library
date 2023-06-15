336a84ec233a2c1230496a0772085fc37ce02e2e
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-15 20:58:48 +0300

    change: add functionality to action buttons above description field

--- src/components/dialogs/TextAreaField.vue
+++ src/components/dialogs/TextAreaField.vue
@@ -3,16 +3,34 @@
     <div class="d-flex justify-space-between">
       <div class="caption d-flex align-end">{{ title }}<span v-if="required" class="red--text"> *</span></div>
       <div class=" pb-1 caption grey--text text--darken-1" v-if="additionalInfo">
-       <!-- {{ additionalInfo }} -->
-       <v-btn fab x-small height="25px" width="25px" elevation="0" text>
-        <v-icon size="17px" color="primary">mdi-text-box-edit-outline</v-icon>
-       </v-btn>
-       <v-btn class="ml-1" fab x-small height="25px" width="25px" elevation="0" text>
-        <v-icon size="17px" color="primary">mdi-text-box-plus-outline</v-icon>
-       </v-btn>
+        <v-btn
+          v-for="button in buttons"
+          :key="button.name"
+          fab
+          x-small
+          height="25px"
+          width="25px"
+          elevation="0"
+          text
+          @click="button.action ? button.action() : () => {}"
+        >
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon
+                size="17px"
+                color="primary"
+                v-bind="attrs"
+                v-on="on">
+                {{ button.icon }}
+              </v-icon>
+            </template>
+            <span> {{ button.tooltip }} </span>
+          </v-tooltip>
+        </v-btn>
       </div>
     </div>
     <v-textarea
+      v-model="item"
       class="ma-0 pa-1 input px-0"
       :label="label"
       :placeholder="label"
@@ -40,7 +58,43 @@ export default {
     required: {
       type: Boolean,
       default: false
+    },
+    value: null
+  },
+  data: function () {
+    return {
+      buttons: [
+        {
+          name: 'Markdown Editor',
+          icon: 'mdi-text-box-edit-outline',
+          tooltip: 'Open Markdown Editor',
+          action: () => {
+            window.open('https://markdown-it.github.io/', '_blank');
+          }
+        },
+        {
+          name: 'Desciption template',
+          icon: 'mdi-text-box-plus-outline',
+          tooltip: 'Paste Description Template',
+          action: () => {
+            if (this.item != null)
+              this.item = this.item + '\n\n---- Description Template ----' +"\n"+this.template
+            else this.item = this.template
+          }
+        }
+      ],
+      template: '## Summary \n The project provides a good test environment for evaluating ... \n ## Setup \n To setup project in Unity, open the CornellBox scene... \n ## Use cases: \n * Can be used to test the general game loop... \n * Can be used to verify that... \n ## Manual Testing Scenarios: \n 1. Build the project for any of target... \n 2. Use Main, TextTest scenes... \n ## Automated Testing Scenarios: \n 1. Run builds with: ``` {EDITOR PATH} -projectPath ... ``` \n ## Yamato CI: \n (Is project integrated in any Yamato CI?) If yes: Provide links \n ## Project showcase: \n Screenshots/videos links that showcase the project and its features'
     }
+  },
+  computed: {
+    item: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    },
   },
 }
 </script>
